07a06d3314580205dbc10931995b9a53614dd56c
Domiee13 <dungngocmd@gmail.com>
2020-08-14 23:31:36 +0700

    Update

---
+++ Codeforces/football.py
@@ -0,0 +1,11 @@
+# Petya loves football very much. One day, as he was watching a football match, he was writing the players' current positions on a piece of paper. To simplify the situation he depicted it as a string consisting of zeroes and ones. A zero corresponds to players of one team; a one corresponds to players of another team. If there are at least 7 players of some team standing one after another, then the situation is considered dangerous. For example, the situation 00100110111111101 is dangerous and 11110111011101 is not. You are given the current situation. Determine whether it is dangerous or not.
+
+s = input()
+
+q = "0000000"
+k = "1111111"
+
+if q in s or k in s:
+    print("YES")
+else:
+    print("NO")
